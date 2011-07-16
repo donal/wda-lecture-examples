607ab4ff98987dfb591dfdfd84887aa4c209eb90
Donal Ellis <donal.ellis@rmit.edu.au>
2011-07-16 18:18:03 +1000

    added example 09

---
+++ 09.one_component_query_2.php
@@ -0,0 +1,12 @@
+<?php
+   require 'db_secure.php';
+
+   // Database activity occurs here -- process $_GET["input"]
+
+   // This is the key to one-component querying:
+   // Redirect the browser back to the calling page, using
+   // the HTTP response header "Location:" and the PHP server
+   // variable $_SERVER["HTTP_REFERER"]
+   header("Location: {$_SERVER["HTTP_REFERER"]}");
+   exit;
+?>
