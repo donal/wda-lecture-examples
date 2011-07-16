4adfba69aaf46dd2d2a9c517c023c04754f8a9bd
Donal Ellis <donal.ellis@rmit.edu.au>
2011-07-16 19:22:43 +1000

    fixed up indenting

--- 10.one_component_query_2.php
+++ 10.one_component_query_2.php
@@ -1,10 +1,10 @@
 <?php
-   // Database activity occurs here -- process $_GET["input"]
+  // Database activity occurs here -- process $_GET["input"]
 
-   // This is the key to one-component querying:
-   // Redirect the browser back to the calling page, using
-   // the HTTP response header "Location:" and the PHP server
-   // variable $_SERVER["HTTP_REFERER"]
-   header("Location: {$_SERVER["HTTP_REFERER"]}");
-   exit;
+  // This is the key to one-component querying:
+  // Redirect the browser back to the calling page, using
+  // the HTTP response header "Location:" and the PHP server
+  // variable $_SERVER["HTTP_REFERER"]
+  header("Location: {$_SERVER["HTTP_REFERER"]}");
+  exit;
 ?>
